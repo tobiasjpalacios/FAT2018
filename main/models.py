906f361cf0c6480babc3adaa474def0426d3f683
tractor <franconbossio@outlook.com>
2018-05-10 13:06:53 -0300

    Elimine calss Person

--- main/models.py
+++ main/models.py
@@ -2,14 +2,10 @@ from django.db import models
 from django.conf import settings
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=32)
-    last_name = models.CharField(max_length=32)
-    personalID = models.IntegerField(unique=True)
-    email = models.EmailField()
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "{} {}".format(self.first_name, self.last_name)
+        return "{} {}".format(self.user.first_name, self.user.last_name)
 
     class Meta:
         abstract = True
