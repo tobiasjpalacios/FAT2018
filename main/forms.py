4a8776d3ba943b445e83927fab7d192614f102d6
tobiasjpalacios <tobiasjpalacios@gmail.com>
2018-05-11 14:14:03 -0300

    forms clasroom creado

--- main/forms.py
+++ main/forms.py
@@ -1,2 +1,6 @@
 from django.forms import ModelForm
 from .models import *
+
+class ClassroomForm(ModelForm):
+    model = Classroom
+    fields = ['day','hour','capacity','duration','name','description']
