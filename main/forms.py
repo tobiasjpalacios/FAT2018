9911a04f12636e271546794fe27ea506dcf76133
Marcos-Corvalan <marcoscorva99@gmail.com>
2018-05-15 10:13:55 -0300

    Se creó el estilo de la tarjeta en CSS.

--- main/forms.py
+++ main/forms.py
@@ -6,7 +6,7 @@ class ClassroomForm(ModelForm):
         model = Classroom
         fields = ['day','hour','capacity','duration','name','description']
 
-class DayForm(ModelForm)
-	class Meta:
-		model = Day
-        fields = ['day', 'start_hour', 'finish_hour', 'interval']
+class DayForm(ModelForm):
+    class Meta:
+        model = Day
+        fields = ['day','start_hour','finish_hour','interval']
