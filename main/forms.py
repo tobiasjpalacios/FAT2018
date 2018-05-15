7ac6c4b6441410a85d8952b7fdaac8cdff40c36e
Marcos-Corvalan <marcoscorva99@gmail.com>
2018-05-15 14:38:55 -0300

    Corrección de stye.css

--- main/forms.py
+++ main/forms.py
@@ -1,12 +1,3 @@
 from django.forms import ModelForm
 from .models import *
 
-class ClassroomForm(ModelForm):
-    class Meta:
-        model = Classroom
-        fields = ['day','hour','capacity','duration','name','description']
-
-class DayForm(ModelForm):
-    class Meta:
-        model = Day
-        fields = ['day','start_hour','finish_hour','interval']
