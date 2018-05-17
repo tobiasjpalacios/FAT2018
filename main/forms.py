55d4e4851a5899a6ebf7be244440f8c13339f08a
tobiasjpalacios <tobiasjpalacios@gmail.com>
2018-05-17 10:25:00 -0300

    eliminando los valores sobrantes

--- main/forms.py
+++ main/forms.py
@@ -1,3 +1,13 @@
+
 from django.forms import ModelForm
 from .models import *
 
+class ClassroomForm(ModelForm):
+    class Meta:
+        model = Classroom
+        fields = ['capacity','name','description']
+
+class DayForm(ModelForm):
+    class Meta:
+        model = Day
+        fields = ['day','start_hour','finish_hour','interval']
