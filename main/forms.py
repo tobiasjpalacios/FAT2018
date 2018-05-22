5f2a4f24fa5e3babee3c883bff8805fc7208479a
tractor <franconbossio@outlook.com>
2018-05-22 10:01:29 -0300

    Create DayFrom

--- main/forms.py
+++ main/forms.py
@@ -1,13 +1,11 @@
-
 from django.forms import ModelForm
 from .models import *
+from django import forms
 
-class ClassroomForm(ModelForm):
-    class Meta:
-        model = Classroom
-        fields = ['capacity','name','description']
-
-class WorkDayForm(ModelForm):
-    class Meta:
-        model = WorkDay
-        fields = ['day' ]
+class DayForm(forms.Form):
+    start_hour = forms.TimeField(label='Hora de inicio')
+    finish_hour = forms.TimeField(label='Hora de finalizacion')
+    day = forms.IntegerField()
+    duration = forms.IntegerField()
+    interval = forms.IntegerField()
+    time_to_start = forms.IntegerField()    
